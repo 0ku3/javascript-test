aeebe1db8fb13fed73eda11c1db1898372ea4578
0ku3 <103297029+0ku3@users.noreply.github.com>
2022-05-18 11:17:33 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -193,4 +193,28 @@ const nameArr = ["タナカ", "ヤマダ", "おっくー"];
 // }
 // console.log(checkSum(50, 40));
 
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2){
+//   console.log("1か2はtrueになります")
+// }
+
+// if (flag1 && flag2){
+//   console.log("1か2もtrueになります")
+// }
+
+// // || は左側がfalseなら右を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
+
 
